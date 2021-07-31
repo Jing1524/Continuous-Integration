3c4233f84b4b42954fe731904025e60b2cc8e1cd
Jing Li <jingli1524@gmail.com>
2021-07-31 11:10:21 -0400

    second commit

--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,13 @@
-describe('foo',()=>{
-    it('baz',()=>{
-        
-    })
-})
+const { expect } = require("chai");
+
+const app = require('supertest')(require('../app'));
+
+describe('Routes',()=>{
+    describe('GET /',()=>{
+        it('Shows information about the api',async()=>{
+            const response = await app.get('/');
+            expect(response.status).to.equal(200);
+            expect(response.text).to.include('Continuous-integration');
+        });
+    });
+});
